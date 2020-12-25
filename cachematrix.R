bb4641839c8885361d0aa6be1e3de54bf87ab275
gayathri05radhakrishnan <76583236+gayathri05radhakrishnan@users.noreply.github.com>
2020-12-25 15:51:12 +0530

    Update cachematrix.R
    assignment

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
   inv<- NULL              #initializing inverse as NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    inv <<- NULL
   }
   get <- function() x    #function to get matrix x
   setinv<- function(inverse)inv<<-inverse
